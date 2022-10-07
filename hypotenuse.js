c9041d503b53e81cbd23df1c722b9ee0154fe0c1
Rohit Shyam Mane <74919679+rohitmane9022@users.noreply.github.com>
2022-10-08 00:22:32 +0530

    Add files via upload

--- hypotenuse.js
+++ hypotenuse.js
@@ -10,7 +10,7 @@ function CalculateSum(a,b){
 function CalculateHypotenuse(){
   const sumofsqures = CalculateSum(Number(Side[0].value),Number(Side[1].value));
   const lengthofHypotenuse = Math.sqrt(sumofsqures)
-  Output.innerText = "The lenght of Hypotenuse "+ lengthofHypotenuse
+  Output.innerText = "The lenght of Hypotenuse "+ lengthofHypotenuse.toFixed(2)
 }
 
 clickbtn.addEventListener('click', CalculateHypotenuse)
